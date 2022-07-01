7de08ccd227e905770369d84b9e5679deff09c19
shangc1016 <shangc1016@163.com>
2022-07-01 17:33:28 +0800

    add parts

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,6 +10,86 @@ typedef struct {
   size_t buffer_length;
   ssize_t input_length;
 } InputBuffer;
+
+typedef enum {
+  META_COMMAND_SUCCESS,
+  META_COMMAND_UNRECOGNIZED_COMMAND
+} MEtaCommandResult;
+
+typedef enum {
+  PREPARE_SUCCESS,
+  PREPARE_UNRECOGNIZED_STATEMENT,
+  PREPAER_SYNTAX_ERROR
+} PrepaerResult;
+
+typedef enum { STATEMENT_INSERT, STATEMENT_SELECT } StatementType;
+
+// 这个是硬编码的这张表的字段
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 255
+
+typedef struct {
+  uint32_t id;
+  char username[COLUMN_USERNAME_SIZE];
+  char email[COLUMN_EMAIL_SIZE];
+} Row;
+// table hard-coded end
+
+// 硬编码的数据库表
+#define size_of_attribute(Struct, Attribute) sizeof(((Struct *)0)->Attribute)
+const uint32_t ID_SIZE = size_of_attribute(Row, id);
+const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
+const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
+const uint32_t ID_OFFSET = 0;
+const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
+const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
+const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
+
+// 一行数据的存取，序列化
+void serialize_row(Row *source, void *destination) {
+  memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
+  memcpy(destination + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
+  memcpy(destination + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
+}
+
+// 反序列化
+void deserialize_row(void *source, Row *destination) {
+  memcpy(&(destination->id), source + ID_OFFSET, ID_SIZE);
+  memcpy(&(destination->username), source + USERNAME_OFFSET, USERNAME_SIZE);
+  memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
+}
+
+//数据库表位置
+const uint32_t PAGE_SIZE = 4096;
+#define TABLE_MAX_PAGES 100
+const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
+const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
+
+// 这个就是数据库表的page指针数组，包括了表的行数；
+typedef struct {
+  uint32_t num_rows;
+  void *pages[TABLE_MAX_PAGES];
+} Table;
+
+// 得到数据库表中某一行的地址；
+void *row_slot(Table *table, uint32_t row_num) {
+  // page_num：行所在的page号
+  uint32_t page_num = row_num / ROWS_PER_PAGE;
+  void *page = table->pages[page_num];
+  if (page == NULL) {
+    //行所在的page不存在，那就分配内存
+    table->pages[page_num] = malloc(PAGE_SIZE);
+    page = table->pages[page_num];
+  }
+  uint32_t row_offset = row_num % ROWS_PER_PAGE;
+  uint32_t byte_offset = row_offset * ROW_SIZE;
+  return page + byte_offset;
+}
+
+typedef struct {
+  StatementType type;
+  Row row_to_insert;  // 这个是插入语句
+} Statement;
 
 InputBuffer *new_input_buffer() {
   InputBuffer *input_buffer = (InputBuffer *)malloc(sizeof(InputBuffer));
@@ -37,19 +118,6 @@ void close_input_buffer(InputBuffer *input_buffer) {
   free(input_buffer);
 }
 
-typedef enum {
-  META_COMMAND_SUCCESS,
-  META_COMMAND_UNRECOGNIZED_COMMAND
-} MEtaCommandResult;
-
-typedef enum { PREPARE_SUCCESS, PREPARE_UNRECOGNIZED_STATEMENT } PrepaerResult;
-
-typedef enum { STATEMENT_INSERT, STATEMENT_SELECT } StatementType;
-
-typedef struct {
-  StatementType type;
-} Statement;
-
 // 得到meta-commands的类型，'.exit'...
 // do_meta_command 现在还是一个wrapper，为后面扩展留出空间
 MEtaCommandResult do_meta_command(InputBuffer *input_buffer) {
@@ -65,6 +133,15 @@ PrepaerResult prepare_statement(InputBuffer *input_buffer,
                                 Statement *statement) {
   if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
     statement->type = STATEMENT_INSERT;
+    // 从输入中解析硬编码的这张表，
+    int args_assigned = sscanf(input_buffer->buffer, "insert %d %s %s",
+                               &(statement->row_to_insert.id),
+                               (char *)&(statement->row_to_insert.username),
+                               (char *)&(statement->row_to_insert.email));
+    // 出错
+    if (args_assigned < 3) {
+      return PREPAER_SYNTAX_ERROR;
+    }
     return PREPARE_SUCCESS;
   }
 
@@ -76,10 +153,21 @@ PrepaerResult prepare_statement(InputBuffer *input_buffer,
 }
 
 //
+void execute_select(Statement *statement, Table *table) {
+  Row row;
+  for (uint32_t i = 0; i < table->num_rows; i++) {
+    deserialize_row(row_slot(table, i), &row);
+    print_row(&row);
+  }
+}
+
+//
 void execute_statement(Statement *statement) {
   switch (statement->type) {
     case (STATEMENT_INSERT):
       printf("this is where we would do an insert.\n");
+      printf("id=%d, username=%s, email=%s\n", statement->row_to_insert.id,
+             statement->row_to_insert.username, statement->row_to_insert.email);
       break;
     case (STATEMENT_SELECT):
       printf("this is where we would do a select.\n");
@@ -111,6 +199,9 @@ int main(int argc, char *argv[]) {
       case (PREPARE_UNRECOGNIZED_STATEMENT):
         printf("Unrecognized keyword at start of '%s'.\n",
                input_buffer->buffer);
+        continue;
+      case (PREPAER_SYNTAX_ERROR):
+        printf("syntax error\n");
         continue;
     }
     execute_statement(&statement);
